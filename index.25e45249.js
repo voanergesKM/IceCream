111221baf1cdc5b4f60b9abd37720d3481dfb80c
voanergesKM <103747805+voanergesKM@users.noreply.github.com>
2022-07-21 12:02:05 +0000

    Deploying to gh-pages from @ voanergesKM/IceCream@3993645b77a544dfffbc2e5b99d4d899674852c0 🚀

---
+++ index.25e45249.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
+//# sourceMappingURL=index.25e45249.js.map
