436785b066c381c32029376ef5a5b9296280c2b5
voanergesKM <103747805+voanergesKM@users.noreply.github.com>
2022-07-21 11:34:15 +0000

    Deploying to gh-pages from @ voanergesKM/IceCream@478f86945749b29a5fff3d459a1109f7fd49aca3 🚀

---
+++ index.f95f709d.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll('a[href^="#"]');for(let t of e)t.addEventListener("click",(function(e){e.preventDefault();const o=t.hasAttribute("href")?t.getAttribute("href"):"body";document.querySelector(o).scrollIntoView({behavior:"smooth",block:"start"})}));
+//# sourceMappingURL=index.f95f709d.js.map
